bef5a17d0b71ecbaffc9f932eb2cf5228cfbbb6b
lukkascost <lucas.costa@lit.ifce.edu.br>
2019-09-25 01:07:36 -0300

    Experiment 2 : Won dataset

--- GENERATE_GLCM.py
+++ GENERATE_GLCM.py
@@ -3,22 +3,21 @@ import numpy as np
 
 from MachineLearn.Classes.Extractors.GLCM import GLCM
 
-MIN_BITS = 4
-MAX_BITS = 12
+MIN_BITS = 8
+MAX_BITS = 8
 
 MIN_DECIMATION = 1
 MAX_DECIMATION = 1
 
-PATH_TO_IMAGES_FOLDER = 'C:/Users/lukka/Desktop/Datasets/Dataset-Fluxo/12b/'
-PATH_TO_SAVE_FEATURES = 'GLCM/EXP_01/'
+PATH_TO_IMAGES_FOLDER = 'D:/Repository/Dataset-Fluxo/Won_data/'
+PATH_TO_SAVE_FEATURES = 'GLCM/EXP_02/'
 
 for nbits in range(MIN_BITS, MAX_BITS + 1):
     for k in range(MIN_DECIMATION, MAX_DECIMATION + 1):
         listGLCM = []
         for quantity in [[1, 352], [2, 382], [3, 378], [4, 382], [5, 376], [6, 360], [7, 361]]:
             for image in range(1, quantity[1] + 1):
-                img = np.array([np.loadtxt(PATH_TO_IMAGES_FOLDER + "C{0}/c{0}_{1}.csv".format(quantity[0], image),
-                                           dtype=int, delimiter=",").T])
+                img = cv2.imread(PATH_TO_IMAGES_FOLDER + "C{0}/c{0}_{1}.png".format(quantity[0], image),0)
                 """ DECIMATION """
                 # klist = [x for x in range(0, img.shape[0], k)]
                 # klist2 = [x for x in range(0, img.shape[1], k)]
@@ -26,7 +25,7 @@ for nbits in range(MIN_BITS, MAX_BITS + 1):
                 # img = img[:, klist2]
 
                 """ CHANGING IMAGE TO VALUES BETWEEN 0 AND  2**NBITS"""
-                img = img / 2 ** (12 - nbits)
+                # img = img / 2 ** (12 - nbits)
 
                 """ APPLYING OTSU'S ALGORITHM """
                 # ret, img = cv2.threshold(img, 0, (2 ** nbits)-1, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
