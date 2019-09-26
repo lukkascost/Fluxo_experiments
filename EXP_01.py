92f44986cd69452aec21a2336b71e1377595edc5
lukkascost <lucas.costa@lit.ifce.edu.br>
2019-09-26 08:47:20 -0300

    Experiment 2 : Won dataset 24 att

--- EXP_01.py
+++ EXP_01.py
@@ -1,3 +1,5 @@
+import sklearn.metrics as sk
+
 import numpy as np
 import cv2
 import cv2.ml as ml
@@ -20,32 +22,35 @@ print(len(classes[classes == 'Class 5']))
 print(len(classes[classes == 'Class 6']))
 print(len(classes[classes == 'Class 7']))
 
-for x, y in enumerate(base):
-    oDataSet.add_sample_of_attribute(np.array(list(np.float32(y)) + [classes[x]]))
-oDataSet.attributes = oDataSet.attributes.astype(float)
-oDataSet.normalize_data_set()
-for j in range(50):
-    print(j)
-    oData = Data(7, 11, samples=47)
-    oData.random_training_test_by_percent([352, 382, 378, 382, 376, 360, 361], 0.8)
-    svm = ml.SVM_create()
-    svm.setKernel(ml.SVM_RBF)
-    oData.params = dict(kernel=ml.SVM_RBF, kFold=2)
-    svm.trainAuto(np.float32(oDataSet.attributes[oData.Training_indexes]), ml.ROW_SAMPLE,
-                  np.int32(oDataSet.labels[oData.Training_indexes]), kFold=2)
-    # svm.train_auto(np.float32(oDataSet.attributes[oData.Training_indexes]),
-    #                np.float32(oDataSet.labels[oData.Training_indexes]), None, None, params=oData.params)
-    results = []  # svm.predict_all(np.float32(oDataSet.attributes[oData.Testing_indexes]))
-    for i in (oDataSet.attributes[oData.Testing_indexes]):
-        res, cls = svm.predict(np.float32([i]))
-        results.append(cls[0])
-    oData.set_results_from_classifier(results, oDataSet.labels[oData.Testing_indexes])
-    oData.insert_model(svm)
-    oDataSet.append(oData)
-oExp.add_data_set(oDataSet,
-                  description="  50 execucoes SVM_{} base Injetoras arquivos em FEATURES_M1_CM8b.txt. ".format(
-                      KERNEL))
-oExp.save("Objects/EXP01_SVM_{}_{}b.gzip".format(KERNEL, MOLD))
+# for x, y in enumerate(base):
+#     oDataSet.add_sample_of_attribute(np.array(list(np.float32(y)) + [classes[x]]))
+# oDataSet.attributes = oDataSet.attributes.astype(float)
+# oDataSet.normalize_data_set()
+# for j in range(50):
+#     print(j)
+#     oData = Data(7, 11, samples=47)
+#     oData.random_training_test_by_percent([352, 382, 378, 382, 376, 360, 361], 0.8)
+#     svm = ml.SVM_create()
+#     svm.setKernel(ml.SVM_RBF)
+#     oData.params = dict(kernel=ml.SVM_RBF, kFold=10)
+#     svm.trainAuto(np.float32(oDataSet.attributes[oData.Training_indexes]), ml.ROW_SAMPLE,
+#                   np.int32(oDataSet.labels[oData.Training_indexes]), kFold=10)
+#     # svm.train_auto(np.float32(oDataSet.attributes[oData.Training_indexes]),
+#     #                np.float32(oDataSet.labels[oData.Training_indexes]), None, None, params=oData.params)
+#     results = []  # svm.predict_all(np.float32(oDataSet.attributes[oData.Testing_indexes]))
+#     for i in (oDataSet.attributes[oData.Testing_indexes]):
+#         res, cls = svm.predict(np.float32([i]))
+#         results.append(cls[0])
+#     oData.set_results_from_classifier(results, oDataSet.labels[oData.Testing_indexes])
+#     print(sk.accuracy_score(oDataSet.labels[oData.Testing_indexes].T[0], np.array(results).T[0]))
+#     print(sk.recall_score(oDataSet.labels[oData.Testing_indexes].T[0], np.array(results).T[0], average=None))
+#
+#     oData.insert_model(svm)
+#     oDataSet.append(oData)
+# oExp.add_data_set(oDataSet,
+#                   description="  50 execucoes SVM_{} base FLUXO GLCM 24att arquivos em FEATURES_M1_CM8b.txt. ".format(
+#                       KERNEL))
+# oExp.save("Objects/EXP01_SVM_{}_{}b.gzip".format(KERNEL, MOLD))
 
 oExp = oExp.load("Objects/EXP01_SVM_{}_{}b.gzip".format(KERNEL, MOLD))
 
